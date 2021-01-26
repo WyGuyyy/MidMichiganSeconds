7ebe053c08be329493fcdf34eef3dc6f80cfff3d
wmtowne <wmtowne@svsu.edu>
2021-01-25 21:46:54 -0800

    Changed backend pointer to heroku backen app

--- src/Services/APIService.js
+++ src/Services/APIService.js
@@ -1,2 +1,2 @@
-//export const baseURI = "https://mid-michigan-seconds-backend.herokuapp.com";
-export const baseURI = "http://localhost:8080";
+export const baseURI = "https://mid-michigan-seconds-backend.herokuapp.com";
+//export const baseURI = "http://localhost:8080";
